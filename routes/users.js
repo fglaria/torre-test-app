83c4aa302d28bc22f385c31f03132fb825a02a94
Felipe Glaría Grego <fglaria@gmail.com>
2021-08-21 17:18:23 -0400

    playing with views & showing user name

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  fetch('https://github.com/')
+    .then(res => res.text())
+    .then(body => console.log(body));
   res.send('respond with a resource');
+});
+
+router.get('/:id', function(req, res, next) {
+  // res.render('user', { id: req.params.id });
+  fetch('https://torre.bio/api/bios/' + req.params.id)
+    .then(res => res.json())
+    // .then(body => console.log(body));
+    .then(body => res.render('user', { id: body.person.name }));
 });
 
 module.exports = router;
