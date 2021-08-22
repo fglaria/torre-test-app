f9ce3deca47e1e72e9eb6c9108fa40353c256308
Felipe Glaría Grego <fglaria@gmail.com>
2021-08-22 19:50:01 -0400

    Backend: Fixed job fetch & result

--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -5,8 +5,10 @@ const fetch = require('node-fetch');
 const api_url =  "https://torre.co/api/suite/opportunities/";
 
 router.get('/:id', function(req, res, next) {
+  console.log(req.params.id);
+  console.log(`${api_url}${req.params.id}`);
   fetch(`${api_url}${req.params.id}`)
-    // .then(res => {console.log(res); res.json()})
+    .then(res => res.json())
     .then(body => res.send(body));
 });
 
