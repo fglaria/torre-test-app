7aab05a76ad1e4a456ea2d58173e0c12b6608d2e
Felipe Glaría Grego <fglaria@gmail.com>
2021-08-21 21:18:15 -0400

    fetching users with POST

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const fetch = require('node-fetch');
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   fetch('https://github.com/')
@@ -14,8 +15,21 @@ router.get('/:id', function(req, res, next) {
   // res.render('user', { id: req.params.id });
   fetch('https://torre.bio/api/bios/' + req.params.id)
     .then(res => res.json())
-    // .then(body => console.log(body));
     .then(body => res.render('user', { id: body.person.name }));
+});
+
+router.get('/search/:param', function(req, res, next) {
+  fetch('https://search.torre.co/people/_search/?offset=20&size=1', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({"size": 1})
+  })
+    .then(res => res.text())
+    .then(body => console.log(body));
+});
+
+router.get('/s/:param', function(req, res, next) {
+  router.post('https://search.torre.co/people/_search/')
 });
 
 module.exports = router;
